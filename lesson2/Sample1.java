5052ab4cd12659b6b75dbf49f124397d055c9b18
sui-0713 <mizuo2015@yahoo.co.jp>
2018-08-25 17:39:37 +0900

    update

--- lesson2/Sample1.java
+++ lesson2/Sample1.java
@@ -1,1 +1,9 @@
-a
+//画面に文字を出力するコード
+class sample1
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello world");
+		System.out.println("wanna be a take me make me  higher");
+	}
+}
