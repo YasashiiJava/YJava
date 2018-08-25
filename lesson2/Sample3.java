e1cc62cfd6cfa1f7da8cea6df0cc438c50a84d1a
sui-0713 <mizuo2015@yahoo.co.jp>
2018-08-25 18:40:23 +0900

    up date

--- lesson2/Sample3.java
+++ lesson2/Sample3.java
@@ -1,1 +1,10 @@
-
+//数字や文字　リテラル
+class sample3
+{
+	 public static void main(String[] args)
+	 {
+	 	System.out.println("COOL");
+	 	System.out.println("おいでやすjava");
+	 	System.out.println("0713");
+	 }
+}
--- lesson2/Sample4.java
+++ lesson2/Sample4.java
@@ -1,1 +1,8 @@
-
+class sample4
+{
+	public static void main(String[] args)
+	{
+		System.out.println("円記号を表します:\\");
+		System.out.println("アポストロフィを表します:\'");
+	}
+}
