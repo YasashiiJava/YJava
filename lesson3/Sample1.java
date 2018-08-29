9f60601332f6f996955b657875187f54593a63cc
sui-0713 <mizuo2015@yahoo.co.jp>
2018-08-29 21:23:29 +0900

    update

--- lesson3/Sample1.java
+++ lesson3/Sample1.java
@@ -1,1 +1,12 @@
+class Sample1
+{
+	public static void main(String[] args)
+	{
+		int num;
 
+		num = 3;
+
+		System.out.println("変数numの値は" + num + "です。");
+
+	}
+}
--- lesson3/Sample2.java
+++ lesson3/Sample2.java
@@ -1,1 +1,15 @@
+class Sample2
+{
+	public static void main(String[] args)
+	{
+		int num = 3;
 
+		System.out.println("変数numの値は "  + num + "です。");
+
+		num = 5;
+
+		System.out.println("変数numの値が進化します。");
+
+		System.out.println("おめでとうございます！変数numの値は "  + num + "になりました！。");
+	}
+}
