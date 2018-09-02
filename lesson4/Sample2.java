804f16447450c0a3360da0ac935552ed3d5343fc
sui-0713 <mizuo2015@yahoo.co.jp>
2018-09-02 20:08:07 +0900

    updete

---
+++ lesson4/Sample2.java
@@ -0,0 +1,20 @@
+class Sample2
+
+{
+	public static void main(String[] args) 
+	{
+		int num1 = 2;
+		int num2 = 3;
+		int sum  = num1+num2;
+
+		System.out.println("変数num1は" + num1 + "です");
+		System.out.println("変数num2は" + num2 + "です");
+		System.out.println("変数num1+num2は" + sum + "です");
+
+		num1 = num1+1;
+
+		System.out.println("変数num1+1は" + num1 + "です");
+
+	}
+
+}
