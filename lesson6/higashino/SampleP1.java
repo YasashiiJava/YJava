bd201d0fef8e4cb61363e55433cc9426bb299276
thigashino <cl525.ht.resolved@gmail.com>
2018-09-09 21:48:41 +0900

    update

--- lesson6/higashino/SampleP1.java
+++ lesson6/higashino/SampleP1.java
@@ -1,1 +1,12 @@
+class SampleP1 {
+	public static void main(String[] args) {
+		int n = 10;
 
+		System.out.println("1～" + n + "までの偶数を出力します．");
+
+		for(int i = 1; i <= n; i++) {
+			if (i % 2 == 0)
+				System.out.println(i);
+		}
+	}
+}
--- lesson6/higashino/SampleP2.java
+++ lesson6/higashino/SampleP2.java
@@ -1,1 +1,19 @@
+import java.io.*;
 
+class SampleP2 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("テストの点数を入力してください．（０で終了）");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+
+        do {
+            String str = br.readLine();
+            int score = Integer.parseInt(str);
+            sum += score;
+        } while (score != 0);
+
+        System.out.println("テストの合計点は" + sum + "点です．");
+    }
+}
