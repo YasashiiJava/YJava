d9be2130a75692c727ea5d7957d8e1e7b6495afa
ThigaFM <31559824+T-Higashino@users.noreply.github.com>
2018-09-10 13:22:21 +0900

    Update SampleP1.java

--- lesson6/higashino/SampleP1.java
+++ lesson6/higashino/SampleP1.java
@@ -4,7 +4,7 @@ class SampleP1 {
 
 		System.out.println("1～" + n + "までの偶数を出力します．");
 
-		for(int i = 1; i <= n; i++) {
+		for (int i = 1; i <= n; i++) {
 			if (i % 2 == 0)
 				System.out.println(i);
 		}
