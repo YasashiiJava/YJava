563bd1039e6303f64e17f2e823a5526423e70e0d
sui-0713 <mizuo2015@yahoo.co.jp>
2018-09-12 20:58:04 +0900

    update

--- lesson5/Sample7.java
+++ lesson5/Sample7.java
@@ -1,1 +1,23 @@
+import java.io.*;
 
+class Sample7
+{
+	public static void main(String[] args) throws IOException{
+		System.out.println("あなたは男ですか？");
+		System.out.println("YかNで答えてください");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+		char res = str.charAt(0);
+
+		if(res == 'Y' || res == 'y'){
+			System.out.println("あなたは男性ですね");
+		}
+		else if (res == 'N' || res == 'n'){
+			System.out.println("おまいはおなご");
+		}
+		else{
+			System.out.println("YかNを入力してください");
+		}
+	}
+}
--- lesson5/Sample8.java
+++ lesson5/Sample8.java
@@ -1,1 +1,18 @@
+import java.io.*;
 
+class Sample8
+{
+	public static void main(String[] args) throws IOException{
+		System.out.println("何番目のコースにするんだ？");
+		System.out.println("好きな数時を選ぶんだな");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+		int res = Integer.parseInt(str);
+
+		char ans = (res == 1 ) ? 'A' : 'B';
+
+		System.out.println(ans + "コースを選択しました");
+		}
+
+}
