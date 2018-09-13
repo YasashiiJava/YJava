9cbb69bf9e603382c08e848d19b8f4a2df280c59
sui-0713 <mizuo2015@yahoo.co.jp>
2018-09-13 22:02:21 +0900

    update

--- lesson5/SampleP2.java
+++ lesson5/SampleP2.java
@@ -1,1 +1,19 @@
+import java.io.*;
 
+class SampleP2
+{
+	public static void main(String[] args) throws IOException{
+		System.out.println("整数を入力してください");
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+		int res = Integer.parseInt(str);
+
+		if((res % 2 ) == 1){
+			System.out.println("奇数が入力されました");
+		}
+		else
+			System.out.println("偶数が入力されました");
+		}
+	}
