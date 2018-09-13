e64758e3a3ef03d1033ad0fd2961cd9895858d32
sui-0713 <mizuo2015@yahoo.co.jp>
2018-09-14 07:39:35 +0900

    update

--- lesson5/SampleP4.java
+++ lesson5/SampleP4.java
@@ -1,1 +1,19 @@
+import java.io.*;
 
+class SampleP4
+{
+	public static void main(String[] args) throws IOException{
+		System.out.println("整数を1から10で入力してください");
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+		int res = Integer.parseInt(str);
+
+		if(res > 0 && res < 11){
+			System.out.println( res + "が入力されました");
+		}
+		else
+			System.out.println("1から10で入力な");
+		}
+	}
--- lesson5/SampleP5.java
+++ lesson5/SampleP5.java
@@ -1,1 +1,31 @@
+import java.io.*;
 
+class SampleP5
+{
+	public static void main(String[] args) throws IOException{
+		System.out.println("貴様の成績を入力してみろ");
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+		int res = Integer.parseInt(str);
+
+		switch(res){
+			case 1:
+			System.out.println("明日があるよ");
+			break;
+		    case 2:
+			System.out.println("やる気スイッチ押そうか");
+			break;
+			case 3:
+			System.out.println("本気出せよ");
+			break;
+			case 4:
+			System.out.println("流石だぜ");
+			break;
+			case 5:
+			System.out.println("お前はやれる奴だと思っていたよ");
+			break;
+		}
+	}
+}
