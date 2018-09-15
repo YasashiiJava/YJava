4ceb9d748d16b537ffdbfc94142ea742b63d26c4
sui-0713 <mizuo2015@yahoo.co.jp>
2018-09-15 11:35:36 +0900

    update

--- lesson6/Sample1.java
+++ lesson6/Sample1.java
@@ -1,1 +1,11 @@
+class Sample1
+{
+	public static void main(String[] args)
+	{
+		for(int i=1; i<=5; i++){
+			System.out.println("繰り返しています");
+		}
 
+		System.out.println("繰り返しは終了しました");
+	}
+}
--- lesson6/Sample2.java
+++ lesson6/Sample2.java
@@ -1,1 +1,12 @@
+class Sample2
+{
+	public static void main(String[] args)
+	{
+		for(int i=1; i<=5; i++){
+			System.out.println(i + "番目の繰り返しです");
+		}
+
+		System.out.println("繰り返しは終了しました");
+	}
+}
 
