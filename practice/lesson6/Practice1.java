48a4e3bda3c6e99c8a745f064d786dac3671def7
sui-0713 <mizuo2015@yahoo.co.jp>
2018-09-19 21:38:24 +0900

    update

--- practice/lesson6/Practice1.java
+++ practice/lesson6/Practice1.java
@@ -1,1 +1,11 @@
+// 3の倍数を表示するループの中に水平タブの指示を入れる
 
+class Practice1
+{
+	public static void main(String[] args)
+	{
+		for(int i = 3; i < 30; i=(i+3)){
+			System.out.print(i + "\t");
+		}
+	}
+}
--- practice/lesson6/Practice2.java
+++ practice/lesson6/Practice2.java
@@ -1,1 +1,17 @@
+// 入力を読み取る機能を付けてiの右側を定数から変数に帰る。
+import java.io.*;
+class Practice2
+{
+	public static void main(String[] args) throws IOException
+	{
+		System.out.println("好きな数を入力してください");
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int num = Integer.parseInt(br.readLine());
+
+		for( int i = num; i < num*10; i=(i+num)){
+			System.out.print(i + "\t");
+		}
+	}
+}
