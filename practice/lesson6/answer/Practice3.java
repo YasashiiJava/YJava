0521bb2f730f11e22ed1617b9d8246f17567c000
ThigaFM <31559824+T-Higashino@users.noreply.github.com>
2018-09-19 23:47:10 +0900

    Update Practice3.java

--- practice/lesson6/answer/Practice3.java
+++ practice/lesson6/answer/Practice3.java
@@ -1,3 +1,19 @@
 class Practice3 {
-
+	public static void main(String[] args) {
+		int a = 1; // 2つ前の項の値を格納する変数
+		int b = 1; // 1つ前の項の値を格納する変数
+		int c;     // 各ループで新しく求めた項の値を格納する変数
+		
+		do {
+			/* フィボナッチ数列の新しい項を求める */
+			c = a + b;
+			
+			/* 次のループに備えてa, bの値を更新する */
+			a = b;
+			b = c;
+			
+			/* 求めた項の値を出力する */
+			System.out.print(c + ", ");
+		} while (c < 1000);
+	}
 }
