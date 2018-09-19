9e2e2520557a943003c3089074aedcaaeb37b32f
ThigaFM <31559824+T-Higashino@users.noreply.github.com>
2018-09-19 15:58:19 +0900

    Update Practice4.java

--- practice/lesson6/answer/Practice4.java
+++ practice/lesson6/answer/Practice4.java
@@ -1,1 +1,22 @@
+import java.io.*;
 
+class Practice4 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// Input size
+		System.out.print("Size: ");
+		int size = Integer.parseInt(br.readLine());
+		
+		// Show X-mark
+		for (int i = 1; i <= size; i++) {
+			for (int j = 1; j <= size; j++) {
+				if (j == i || j == size - i)
+					System.out.print("X");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
