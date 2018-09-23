09e74b564b524bd8fc559f899540ade22a660120
sui-0713 <mizuo2015@yahoo.co.jp>
2018-09-23 18:19:54 +0900

    update

--- lesson7/Sample9.java
+++ lesson7/Sample9.java
@@ -1,1 +1,26 @@
+import	java.io.*;
+
+class Sample2
+{
+	public static void main(String[] args) throws IOException
+	{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+		int num = Integer.parseInt(str);
+
+		int[] test = new int [5];
+		System.out.println(test.length + "人の得点を入力してください");
+
+			for(int i = 0; i < test.length; s++){
+				str = br.readLine();
+				test[i] = Integer.parseInt(str);
+			}
+
+			for(int s = 0; s <test.length - 1; s++){
+			System.out.println((i + 1) + "番目の人の点数は" + test[i] + "です");
+		}
+	}
+}
 
