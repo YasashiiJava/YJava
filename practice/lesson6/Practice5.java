9f8fc44dc735b6f8843812ec0388b69474ad9b35
ThigaFM <31559824+T-Higashino@users.noreply.github.com>
2018-09-28 13:49:51 +0900

    Update Practice5.java

--- practice/lesson6/Practice5.java
+++ practice/lesson6/Practice5.java
@@ -1,6 +1,12 @@
 import java.io.*;
+import java.util.Random;
+
 class Practice5 {
 	public static void main(String[] args) throws IOException {
-		System.out.println("好きな数を入力してください");
+		/* 答えとなる数字を乱数で決める */
+		Random rnd = new Random();
+		int ans = rnd.nextInt(10);  // ans: answerの略
+		
+		/***** 以下，自分で実装せよ *****/
 	}
 }
