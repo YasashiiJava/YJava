908a39a56eddb081e25796d23f153cd9a3fb23d5
ThigaFM <31559824+T-Higashino@users.noreply.github.com>
2018-09-28 14:22:04 +0900

    Update Practice3.java

--- practice/lesson7/Practice3.java
+++ practice/lesson7/Practice3.java
@@ -1,1 +1,19 @@
+import java.io.*;
 
+class Practice3 {
+	public static void main(String[] args) throws IOException {
+		/* 九九の表を保存する二次元配列を用意 */
+		int[][] kuku = new int[9][9];
+		
+		/* 表の中身を作る */
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				// hogehoge
+			}
+		}
+		
+		/* 2つの数字（1 ~ 9）を入力して，掛け算した値を表示する */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// piyopiyo
+	}
+}
