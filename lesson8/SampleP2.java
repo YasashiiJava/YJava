4649ec83dda70a4e5a36692d49bbcda384f74964
sui-0713 <mizuo2015@yahoo.co.jp>
2018-09-30 18:40:39 +0900

    update

--- lesson8/SampleP2.java
+++ lesson8/SampleP2.java
@@ -1,1 +1,32 @@
+//SampleP1 ①○②○③×
 
+//車クラス
+class Car
+{
+	int num;
+	double gas;
+
+	void setNumGas(int n, double g)
+	{
+		num = n;
+		gas = g;
+		System.out.println("車のナンバーを" + num + "にガソリン量を" + gas + "にしました");
+	}
+	void show()
+	{
+		System.out.println("車のナンバーは" + num + "にしました");
+		System.out.println("にガソリン量は" + gas + "にしました");
+	}
+}
+
+class SampleP2
+{
+	public static void main(String[] args)
+	{
+		Car car1 = new Car();
+		car1.setNumGas(1234, 20.5);
+		car1.show();
+	}
+}
+
+//SampleP3 イ
--- lesson8/SampleP5.java
+++ lesson8/SampleP5.java
@@ -1,1 +1,38 @@
+//座標のクラス
+class Mypoint
+{
+	int x;
+	int y;
+	//Mypointは上記の2つのフィールドから成り立つ
 
+	int getX()
+	{
+		System.out.println("X座標を得ます");
+		return x;
+	}
+	int getY()
+	{
+		System.out.println("Y座標を得ます");
+		return y;
+	}
+	void setXY(int px, int py)
+	{
+		x = px;
+		y = py;
+		System.out.println("X座標とY座標の出力準備をします");
+	}
+}
+
+class SampleP5
+{
+	public	static void main(String[] args)
+	{
+		Mypoint point1 = new Mypoint();
+		point1.setXY(5, 10);
+
+		int xx = point1.getX();
+		int yy = point1.getY();
+
+		System.out.println("X座標は" + xx + ":Y座標は" + yy + "です");
+	}
+}
