4869ea696110cceb2bb5123f0934d2323e4f7a01
sui-0713 <mizuo2015@yahoo.co.jp>
2018-10-02 07:03:07 +0900

    update

--- practice/lesson6/Practice4.java
+++ practice/lesson6/Practice4.java
@@ -1,4 +1,6 @@
 //入力された数字の分繰り返す→行をnumとnum-2で出力 + Xはi行目のi番目にあるそして斜めも同様にななめに出す。
+import java.io.*;
+
 class Practice4
 {
 	public static void main(String[] args) throws IOException
@@ -7,19 +9,18 @@ class Practice4
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		String str = br.readLine();
 		int num = Integer.parseInt(br.readLine());
 
-		for(int i = 1; i < num; i++) {
-			System.out.println("");
-			for(int j = 1; j <= num +1; j++) {
-				if(j == i) {
+		for(int i = 1; i <= num; i++) {
+			for(int j = 1; j <= num; j++) {
+				if(j == i || j == num - i + 1) {
 					System.out.print("X");
 				}
 				else {
 					System.out.print(" ");
 				}
 			}
+			System.out.println();
 		}
 	}
 }
--- practice/lesson6/Practice5.java
+++ practice/lesson6/Practice5.java
@@ -5,8 +5,33 @@ class Practice5 {
 	public static void main(String[] args) throws IOException {
 		/* 答えとなる数字を乱数で決める */
 		Random rnd = new Random();
-		int ans = rnd.nextInt(10);  // ans: answerの略
-		
+		int ans = rnd.nextInt(7);  // ans: answerの略
+
 		/***** 以下，自分で実装せよ *****/
+
+		System.out.println("game start!");
+
+		// 入力の指示を出す
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// for iで5回の入力
+		for(int i = 0; i <= 4; i++) {
+			System.out.print("input;0~9  ");
+			String str = br.readLine();
+			int inp = Integer.parseInt(str);
+
+			if(ans == inp) {
+				System.out.println("you win!!");
+				System.out.println("Thank you for playing!");
+				break;
+			}
+			else if(i == 4) {
+				System.out.println("you lose...");
+				System.out.println("Thank you for playing!");
+			}
+			else {
+				System.out.println("Your Input is Different!");
+			}
+		}
 	}
 }
