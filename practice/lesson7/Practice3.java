d06e5550b789cd3af04e34b6bcc77bc3c3420e23
sui-0713 <mizuo2015@yahoo.co.jp>
2018-10-04 07:05:02 +0900

    update

--- practice/lesson7/Practice3.java
+++ practice/lesson7/Practice3.java
@@ -4,16 +4,25 @@ class Practice3 {
 	public static void main(String[] args) throws IOException {
 		/* 九九の表を保存する二次元配列を用意 */
 		int[][] kuku = new int[9][9];
-		
+
 		/* 表の中身を作る */
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				// hogehoge
+				kuku[i][j] = (i + 1) * (j + 1);
 			}
 		}
-		
 		/* 2つの数字（1 ~ 9）を入力して，掛け算した値を表示する */
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// piyopiyo
+		System.out.println("かける数とかけられる数を入力してください");
+
+		String str1 = br.readLine();
+		String str2 = br.readLine();
+		i = Integer.parseint(str1);
+		j = Integer.parseint(str2);
+
+		System.out.println(i + "かける" + j + "は" + kuku[i][j] + "です");
+
 	}
 }
--- practice/lesson7/Practice4.java
+++ practice/lesson7/Practice4.java
@@ -1,1 +1,12 @@
+// ①半径1cmの四角形を作る ②少数の乱数を発生させる ③四分円を作り中心からrまでの距離以内の点の数を数える
 
+import java.lang.math;
+
+class Hoge
+{
+	public static void main(String[] args)
+	{
+		double px = Math.random();
+		double py = Math.random();
+	}
+}
