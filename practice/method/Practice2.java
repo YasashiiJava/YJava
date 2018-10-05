f988483a2b8165596251c439eb9695e98d876ea3
ThigaFM <31559824+T-Higashino@users.noreply.github.com>
2018-10-06 00:11:57 +0900

    Update Practice2.java

--- practice/method/Practice2.java
+++ practice/method/Practice2.java
@@ -1,1 +1,20 @@
-
+class Practice2 {
+	/* ここにメソッドを実装してね */
+	
+	
+	public static void main(String[] args) {
+		/* 以下でメソッドのテストをする */
+		
+		// total(5) = 15
+        System.out.println("total(5) = " + total(5));
+        
+		// total(1) = 1
+        System.out.println("total(1) = " + total(1));
+        
+		// total(10) = 10
+        System.out.println("total(10) = " + total(10));
+        
+		// total(100) = ???
+        System.out.println("total(100) = " + total(100));
+    }
+}
