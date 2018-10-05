06e35719e861595e31a1591725d5b3601d99b264
ThigaFM <31559824+T-Higashino@users.noreply.github.com>
2018-10-06 00:23:56 +0900

    Update Practice4.java

--- practice/method/Practice4.java
+++ practice/method/Practice4.java
@@ -1,1 +1,26 @@
+class Practice4 {
+	/* ここにメソッドを実装してね */
 
+
+	public static void main(String[] args) {
+		/* 以下でメソッドのテストを行う */
+
+		// fibonacci(1) = 1
+		System.out.println("fibonacci( 1) = " + fibonacci(1));
+			
+		// fibonacci(2) = 1
+		System.out.println("fibonacci( 2) = " + fibonacci(2));
+
+		// fibonacci(3) = 2
+		System.out.println("fibonacci( 3) = " + fibonacci(3));
+
+		// fibonacci(4) = 3
+		System.out.println("fibonacci( 4) = " + fibonacci(4));
+
+		// fibonacci(7) = 13
+		System.out.println("fibonacci( 7) = " + fibonacci(7));
+
+		// fibonacci(10) = 54
+		System.out.println("fibonacci(10) = " + fibonacci(10));
+	}
+}
