c1cbac8992385249d1c040d68aafd33185bf0b17
ThigaFM <31559824+T-Higashino@users.noreply.github.com>
2018-10-06 00:08:37 +0900

    Update Practice1.java

--- practice/method/Practice1.java
+++ practice/method/Practice1.java
@@ -1,1 +1,24 @@
-
+class Practice1 {
+	/* ここにメソッドを実装してね */
+	
+	
+	public static void main(String[] args) {
+		/* 以下でメソッドのテストをする */
+		
+		// 15の2倍は30です的な文が出力されればOK
+		int result1 = mul(3, 5);
+		printDouble(result1);
+		
+		// 18782の2倍は37564
+		int result2 = mul(1, 18782);
+		printDouble(relult2);
+		
+		// -9の2倍は-18
+		int result3 = mul(-3, 3);
+		printDouble(relult3);
+		
+		// 0の2倍は0
+		int result4 = mul(0, 0);
+		printDouble(relult4);
+	}
+}
