693bd5232017df809d86a4402d76c778d6d4359b
sui-0713 <mizuo2015@yahoo.co.jp>
2018-10-08 18:12:39 +0900

    update

--- lesson9/SampleP2.java
+++ lesson9/SampleP2.java
@@ -1,1 +1,41 @@
+class Car
+{
+	public static int sum = 0;
 
+	private int num;
+	private double gas;
+
+	public Car()
+	{
+		num = 0;
+		gas = 0.0;
+		sum++;
+		System.out.println("車を作成しました");
+	}
+	public void setCar(int n, double g)
+	{
+		num = n;
+		gas = g;
+		System.out.println("ナンバーを" + num + "に、ガソリン量は" + gas + "にしました");
+	}
+	public static void showSum()
+	{
+		System.out.println("車は全部で" + sum + "台あります");
+	}
+	public void show()
+	{
+		System.out.println("車のナンバーは" + num + "で、ガソリン量は" + gas + "です");
+	}
+}
+
+class SampleP2
+{
+	public static void main(String[] args)
+	{
+		Car car1 = new Car();
+		car1.setCar(1234, 20.5);
+
+		Car.showSum();
+		car1.show();
+	}
+}
--- lesson9/SampleP5.java
+++ lesson9/SampleP5.java
@@ -1,1 +1,61 @@
+//座標のクラス
+class MyPoint
+{
+	private int x;
+	private int y;
+	//MyPointは上記の2つのフィールドから成り立つ
+
+	public void Mypoint()
+	{
+		x = 0;
+		y = 0;
+		System.out.println("初期座標を" + x + "." + y + "にします");
+	}
+
+	public int getX()
+	{
+		System.out.println("X座標を得ます");
+		return x;
+	}
+	public int getY()
+	{
+		System.out.println("Y座標を得ます");
+		return y;
+	}
+	public void setX(int px)  // メソッドを2つに分けるように修正
+	{
+		if(0 <= px && px <= 100) {
+			x = px;
+			System.out.println("X座標の出力準備をします");
+		}
+		else {
+			System.out.println("０～１００で入力してね");
+		}
+	}
+	public void setY(int py)  // メソッドを2つに分けるように修正
+	{
+		if(0 <= py && py <= 100) {
+			y = py;
+			System.out.println("Y座標の出力準備をします");
+		}
+		else {
+			System.out.println("０～１００で入力してね");
+		}
+	}
+}
+
+class SampleP5
+{
+	public	static void main(String[] args)
+	{
+		MyPoint point1 = new MyPoint();
+		point1.setX(5);
+		point1.setY(10);
+
+		int v = point1.getX();
+		int w = point1.getY();
+
+		System.out.println("X座標は" + v + ":Y座標は" + w + "です");
+	}
+}
 
