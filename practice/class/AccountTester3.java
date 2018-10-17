d767b89da825fa78d8adb7c3d363349c4531d9c9
ThigaFM <31559824+T-Higashino@users.noreply.github.com>
2018-10-18 00:08:58 +0900

    Create AccountTester3.java

---
+++ practice/class/AccountTester3.java
@@ -0,0 +1,28 @@
+// 銀行口座クラス【第2版】とそれをテストするクラス
+
+// 銀行口座クラス【第2版】
+/******************* ここにクラス設計を記述 *******************/
+
+
+/******************* ここより下はいじらない *******************/
+
+// 銀行口座クラスをテストするクラス
+class AccountTester2 {
+	public static void main(String[] args) {
+		Account adachi = new Account("足立幸一", "123456", 1000);  // 足立君の口座
+		Account nakata = new Account("仲田真二", "654321", 200);  // 仲田君の口座
+		
+		adachi.balance -= 200;  // 足立君が200円おろす
+		nakata.balance += 100;  // 仲田君が100円預ける
+		
+		System.out.println("■足立君の口座");
+		System.out.println("　口座名義：" + adachi.name);
+		System.out.println("　口座番号：" + adachi.no);
+		System.out.println("　預金残高：" + adachi.balance);
+		
+		System.out.println("■仲田君の口座");
+		System.out.println("　口座名義：" + nakata.name);
+		System.out.println("　口座番号：" + nakata.no);
+		System.out.println("　預金残高：" + nakata.balance);
+	}
+}
