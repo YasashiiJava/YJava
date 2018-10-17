ee0d802539621f58e0e9e341ba1f3a33e9f19d7f
ThigaFM <31559824+T-Higashino@users.noreply.github.com>
2018-10-18 00:10:25 +0900

    Update AccountTester3.java

--- practice/class/AccountTester3.java
+++ practice/class/AccountTester3.java
@@ -7,22 +7,22 @@
 /******************* ここより下はいじらない *******************/
 
 // 銀行口座クラスをテストするクラス
-class AccountTester2 {
+class AccountTester3 {
 	public static void main(String[] args) {
 		Account adachi = new Account("足立幸一", "123456", 1000);  // 足立君の口座
 		Account nakata = new Account("仲田真二", "654321", 200);  // 仲田君の口座
 		
-		adachi.balance -= 200;  // 足立君が200円おろす
-		nakata.balance += 100;  // 仲田君が100円預ける
+		adachi.withdraw(200);  // 足立君が200円おろす
+		nakata.deposit(100);   // 仲田君が100円預ける
 		
 		System.out.println("■足立君の口座");
-		System.out.println("　口座名義：" + adachi.name);
-		System.out.println("　口座番号：" + adachi.no);
-		System.out.println("　預金残高：" + adachi.balance);
+		System.out.println("　口座名義：" + adachi.getName());
+		System.out.println("　口座番号：" + adachi.getNo());
+		System.out.println("　預金残高：" + adachi.getBalance());
 		
 		System.out.println("■仲田君の口座");
-		System.out.println("　口座名義：" + nakata.name);
-		System.out.println("　口座番号：" + nakata.no);
-		System.out.println("　預金残高：" + nakata.balance);
+		System.out.println("　口座名義：" + nakata.getName());
+		System.out.println("　口座番号：" + nakata.getNo());
+		System.out.println("　預金残高：" + nakata.getBalance());
 	}
 }
